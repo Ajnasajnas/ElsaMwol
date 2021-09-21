3fda4b8d9cfb0b7d9673892b0d68f067ed887623
ElsaMwol <87955538+Ajnasajnas@users.noreply.github.com>
2021-09-21 17:47:39 +0530

    Update git.js

--- plugins/git.js
+++ plugins/git.js
@@ -18,12 +18,16 @@ Asena.addCommand({pattern: 'git', fromMe: false, desc: Lang.WP}, (async (message
 
     var respoimage = await axios.get(`${r_text[i]}`, { responseType: 'arraybuffer' })
 
-    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: `*My creator is Ameer Suhail*
-*Creator number : wa.me/919946432377?text=Hi%20Ameer%20Suhail.%20*
-*Bot setting video : https://youtu.be/_D4ZYuUSXjs*
+    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: `*My creator is AJNAS*
+*Creator number : wa.me/917736936605?text=Hi%20Muhammed%20Ajnas.%20*
+*Bot setting video : https://youtu.be/L8E4enqfYik*
+
 *Githublink (Setup)  :    https://github.com/ameer-kallumthodi/pikachu*
-*Audio commads :   https://github.com/ameer-kallumthodi/pikachu/tree/master/uploads*
-*Sticker commads : https://github.com/ameer-kallumthodi/pikachu/tree/master/uploads*
+
+*Audio commads :   https://github.com/Ajnasajnas/Elsa/tree/master/uploads*
+
+*Sticker commads : https://github.com/Ajnasajnas/Elsa/tree/master/uploads*
+
 `}) 
 
 }));
